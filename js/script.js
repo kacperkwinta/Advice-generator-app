9e48ac8a52ae78ce35b19a41b7cfc307ab3140b0
Kacper Kwinta <kaspersky.kwinta@gmail.com>
2023-09-18 22:47:50 +0200

    v1 done

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const apiUrl = "https://api.api-ninjas.com/v1/quotes?";
+const apiKey = "2TidqqXkCjVE08hf7pDsYQ==bJmQVy2ku63YTUvq";
+const quoteElement = document.querySelector(".quote");
+const categoryElement = document.querySelector(".category");
+const btn = document.querySelector(".btn");
+
+async function fetchQuote() {
+	try {
+		const response = await fetch(apiUrl, {
+			headers: {
+				"X-Api-Key": apiKey,
+				"Content-Type": "application/json",
+			},
+		});
+
+		if (!response.ok) {
+			throw new Error(`Error: ${response.status} - ${response.statusText}`);
+		}
+
+		const data = await response.json();
+		const { quote, category } = data[0];
+		quoteElement.textContent = quote;
+		categoryElement.textContent = `#${category}`;
+		console.log(data);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+btn.addEventListener("click", fetchQuote);
